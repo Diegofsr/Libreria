7ea9ec3c43eecf3ae7ed93042119993a4dacf492
Diego <diegofsantosr@hotmail.com>
2026-04-30 10:55:35 +0200

    Subiendo la clase Biblioteca

--- src/main/java/org/example/Biblioteca.java
+++ src/main/java/org/example/Biblioteca.java
@@ -1,4 +1,23 @@
 package org.example;
+import java.util.ArrayList;
 
 public class Biblioteca {
+
+    private Libro[] estanteria;
+    private int cantidadLibros;
+
+    public Biblioteca() {
+        this.estanteria = new Libro[10];
+        this.cantidadLibros = 0;
+    }
+
+    public void agregarLibro(Libro nuevoLibro) {
+        if (cantidadLibros < estanteria.length) {
+            estanteria[cantidadLibros] = nuevoLibro;
+            cantidadLibros++;
+            System.out.println("Libro colocado en la estantería. Cantidad de libros " + cantidadLibros);
+        } else {
+            System.out.println("No caben más libros, estantería llena.");
+        }
+    }
 }
